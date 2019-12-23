3b2fb8a23eb27d31e03dbd421d2d8500c4097c90
Mikhail Marenov <enf644@gmail.com>
2019-12-23 20:30:39 +0300

    Add files via upload

--- PitchPurchaseRequest/action_16d0ba4b-0578-45da-9321-7f6c9ed24f36.py
+++ PitchPurchaseRequest/action_16d0ba4b-0578-45da-9321-7f6c9ed24f36.py
@@ -1,9 +1,17 @@
-if ax.row.catItems:
-    cat_guids_str = ', '.join(f'"{k}"' for k in ax.row.catItems)
-    sql_query = f'SELECT * FROM "PitchPurchaseRequestItems" WHERE guid IN({cat_guids_str})'
-    total = 0
-    result = ax.sql(sql_query)
-    if result:
-        for row in result:
-            total += float(row['quantity']) * float(row['indicativePrice'])
+if ax.row.catItems:
+
+    cat_guids_str = ', '.join(f'"{k}"' for k in ax.row.catItems)
+
+    sql_query = f'SELECT * FROM "PitchPurchaseRequestItems" WHERE guid IN({cat_guids_str})'
+
+    total = 0
+
+    result = ax.sql(sql_query)
+
+    if result:
+
+        for row in result:
+
+            total += float(row['quantity']) * float(row['indicativePrice'])
+
         ax.row.total = total
